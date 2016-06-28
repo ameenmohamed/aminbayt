6610279bab53a08a4747fbcd3ec06c5e88eb6871
amin.mohammed@gmail.com <amin.mohammed@gmail.com>
2016-06-29 00:26:28 +0100

    firstCommit

---
+++ springcloud/src/main/java/ie/hac/cloudserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ie.hac.cloudserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringcloudApplication.class);
+	}
+
+}
